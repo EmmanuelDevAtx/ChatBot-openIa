61708ee67c00f0e3a26c797a63cb695b1a481828
Emmanuel <emejia@atx.mx>
2023-02-23 16:40:46 -0600

    Add the api Open IA

--- App.tsx
+++ App.tsx
@@ -4,10 +4,36 @@ import React, { useState } from 'react';
 import { Button, Card, Provider as PaperProvider, Text, TextInput } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: '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',
+});
+import "react-native-url-polyfill/auto";
 
+const openai = new OpenAIApi(configuration);
 
 const App = () => {
   const [question, setQuestion] = useState("");
+  const [questionEnable, setQuestionEnable]=useState(false);
+  const [repsonseIA, setResponseIA] = useState("");
+
+
+
+  const openIA=async()=>{
+    setQuestionEnable(true);
+    setResponseIA("");
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: question,
+      temperature: 0.5,
+      max_tokens: 300,
+    });
+    const datos = response.data.choices as JSON;
+    setQuestion('');
+    setQuestionEnable(false);
+    setResponseIA(datos[0].text);
+    
+  }
 
   return (
         
@@ -19,15 +45,19 @@ const App = () => {
           <Card.Content >
             <Text style={{alignSelf:'center', fontSize:20, marginVertical:10}}>Welcom to Chat bot</Text>
             <TextInput
+              disabled = {questionEnable}
               mode='outlined'
               label="Enter your question"
               value={question}
               onChangeText={text => setQuestion(text)}
               style={{marginVertical:15}}
             />
-          <Button  mode='outlined' onPress={()=>{}}> Ask OpenIA</Button> 
+          <Button  mode='outlined' onPress={()=>openIA()}> Ask OpenIA</Button> 
+
+          
           </Card.Content>
         </Card>
+        <Text style={{alignSelf:'center', fontSize:14, marginVertical:10}}>{repsonseIA}</Text>
       </SafeAreaView>
    </PaperProvider>
     
