59d6460cfbcf8bc0e9ee5be1951c861a5ecc50c1
EmmanuelDevAtx <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-03-15 21:51:00 -0600

    Add style to the app

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 
 import React, { useEffect, useState } from 'react';
-import { Clipboard, FlatList, Image, ScrollView, View } from 'react-native';
+import { Clipboard, FlatList, Image, ScrollView, StyleSheet, View } from 'react-native';
 
-import { Button, Card, Provider as PaperProvider, Text, TextInput } from 'react-native-paper';
+import { Button, Card, Provider as PaperProvider, Text, TextInput, MD3DarkTheme as DefaultTheme, AnimatedFAB, } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -10,6 +10,14 @@ import Rive from 'rive-react-native';
 import "react-native-url-polyfill/auto";
 import { openIA } from './src/helpers/ResponseIA';
 
+import { Banner } from 'react-native-paper';
+import { colors } from './src/Theme/Colors';
+
+// const theme = {...DefaultTheme, color:{
+//   primary: '#66EAFF'
+// }};
+
+const theme = {...DefaultTheme, ...colors};
 
 type responseInfo={
   questionUser: string,
@@ -18,6 +26,7 @@ type responseInfo={
 }
 
 const App = () => {
+  const [showQuestion, setShhowQuestion] = useState(false);
   const [question, setQuestion] = useState("");
   const [isLoadingResponse, setIsLoadingResponse] = useState(false);
   const [responseArrayIA, setResponseArrayIA]=useState<any[]>([]);
@@ -81,7 +90,7 @@ const App = () => {
   }) => (
     <View style={{marginVertical:20 , paddingHorizontal:20
     }}>
-    <Card onPress={()=>cliBoard(response)}>
+    <Card onPress={()=>cliBoard(response)} style={styles.cardResponse}>
           <Card.Content >
            <Text style={{alignSelf:'flex-start', fontSize:19, marginVertical:0, flex:1}}>{questionUser}</Text>
             <Text style={{alignSelf:'center', fontSize:15, marginVertical:0, flex:1}}>{response}</Text>
@@ -98,31 +107,50 @@ const App = () => {
         </Card>
     </View>
   );
+  //colores 
+  // 120536
+// 2D208A
+// A70984
+// 15D8FB
+
+
 
   return (
-    <PaperProvider>  
+    <PaperProvider theme={theme}>  
       <SafeAreaView style={{justifyContent:'center', alignContent:'center', flex:1 }}>
-        <LinearGradient colors={['#CEEFF7','#7DBCFD','#3223BA']} style={{height:'100%'}}>
-        <Card  style={{}}>
-          <Card.Content >
-            <Text style={{alignSelf:'center', fontSize:20, marginVertical:10}}>Welcom to Chat bot</Text>
-            <TextInput
+        <LinearGradient colors={['#120536','#2D208A']} style={{height:'100%'}}>
+        <Banner
+        style={{...styles.cardResponse, borderRadius:0, borderBottomWidth:2,borderWidth:0, backgroundColor:'rgba(19, 19, 19, 0.5)',}}
+      visible={showQuestion}
+
+      icon={({size}) => (
+        <View >
+          <Text style={{alignSelf:'center', fontSize:20, marginVertical:10}}>Welcom to Chat bot</Text>
+          <TextInput
               disabled = {isLoadingResponse}
               mode='outlined'
               label="Enter your question"
               value={question}
               onChangeText={text => setQuestion(text)}
-              style={{marginVertical:15}}
+              style={{marginVertical:15, width: 330, height: 50, alignSelf:'center'}}
             />
-          <Button  mode='outlined' onPress={()=>
+
           {
-            ask(question);
-          }} style={{marginVertical:5}}> Ask OpenIA</Button>
-          <Button  mode='outlined' onPress={async()=>{
-              console.log('tus datos osn' ,lastInformation);
-            }} style={{marginVertical:5}}> Regenerate Response</Button> 
-          </Card.Content>
-        </Card>
+            !isLoadingResponse 
+            ? <Button  mode='outlined' onPress={()=>
+              {
+                ask(question);
+              }} style={{marginVertical:5}}> Ask OpenIA</Button>
+            : <></>
+          }
+
+        </View>
+        
+      )}
+      >
+        
+    </Banner>
+        
         <ScrollView>
         <View style={{marginVertical:20 , paddingHorizontal:20
     }}>
@@ -130,7 +158,7 @@ const App = () => {
           
           lastInformation.questionUser !== 'none'
           ?
-          <Card >
+          <Card onPress={()=>cliBoard(lastInformation.response)} style={styles.cardResponse}>
           <Card.Content >
               <Text style={{alignSelf:'flex-start', fontSize:19, marginVertical:0}}>{lastInformation.questionUser}</Text>
              <Text style={{alignSelf:'center', fontSize:15, marginVertical:0}}>{lastInformation.response}</Text>
@@ -153,7 +181,7 @@ const App = () => {
             {isLoadingResponse ? 
       <View style={{marginVertical:20 , paddingHorizontal:20
       }}>
-        <Card>
+        <Card style={styles.cardResponse}>
 
          <Card.Content >
           <Rive
@@ -187,10 +215,47 @@ const App = () => {
       }
         
         </ScrollView>
+        <AnimatedFAB
+        icon={({size}) => (
+          <Text style={{color:'black', alignSelf:'center'}}>{!showQuestion ?  'Open IA' : 'Hide IA'}</Text>
+          // <></>
+        )}
+        
+        color='black'
+        label={''}
+        extended={false}
+        onPress={() => setShhowQuestion(!showQuestion)}
+        visible={true}
+        animateFrom={'right'}
+        iconMode={'static'}
+        style={styles.fabStyle}
+      />
         </LinearGradient>
       </SafeAreaView>
    </PaperProvider>
     
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+  },
+  fabStyle: {
+    bottom: 16,
+    // right: 16,
+    alignSelf:'center',
+    position: 'absolute',
+    backgroundColor:'white',
+    color:'black'
+  },
+  cardResponse:{
+    backgroundColor:'rgba(19, 19, 19, 0.2)', 
+    borderRadius:20, 
+    shadowColor:'rgba(0, 0, 0, 0.0)',
+    borderColor:'#FFFFFF',
+    borderWidth:1,
+  }
+});
 export default App;
