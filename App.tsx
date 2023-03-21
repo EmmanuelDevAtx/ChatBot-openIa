9a1182e5bc0d3de1262d4495cfc441a2f51b69f6
EmmanuelDevAtx <120133182+EmmanuelDevAtx@users.noreply.github.com>
2023-03-20 23:44:41 -0600

    Add full splash screen

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@
 import React, { useEffect, useState } from 'react';
 import { Clipboard, FlatList, Image, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import { Button, Card, Provider as PaperProvider, TextInput, MD3DarkTheme as DefaultTheme, AnimatedFAB, } from 'react-native-paper';
+import {Provider as PaperProvider, MD3DarkTheme as DefaultTheme, } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import LinearGradient from 'react-native-linear-gradient';
+import SplashScreen from 'react-native-splash-screen'
 
 import "react-native-url-polyfill/auto";
 
@@ -15,12 +16,14 @@ import { HomeScreen } from './src/screens/homeScreen';
 const theme = {...DefaultTheme, ...colors};
 
 const App = () => {
+  useEffect(() =>{
+    setTimeout(() => SplashScreen.hide(), 1000);
+  },[])
   return (
     <PaperProvider theme={theme}>  
       <SafeAreaView style={{justifyContent:'center', alignContent:'center', flex:1 }}>
         <LinearGradient colors={['#29313B','#1A1E23']} style={{height:'100%'}}>
           <HomeScreen/>
-          
         </LinearGradient>
       </SafeAreaView>
    </PaperProvider>
